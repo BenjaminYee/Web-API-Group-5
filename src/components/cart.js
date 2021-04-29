bf867f7f6709a1386e7d5a89edf5fbdc4394f32b
An4123 <anvo2016026@gmail.com>
2021-04-29 15:51:31 -0600

    added cart functionality

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,45 +1,39 @@
 import React, { Component } from 'react';
-import { fetchFoods } from "../actions/foodActions";
-import { setFood } from "../actions/foodActions";
+import {CartArray} from "../actions/foodActions";
 import {connect} from 'react-redux';
-import { Card, Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import { render } from '@testing-library/react';
 
 
 
 class Cart extends Component{
-    constructor(props){
-        super(props)
-        this.handlesSelect = this.handlesSelect.bind(this)
-    }
-
-
-
-
     render(){
-        const CartListCards = ({cartList}) => {
-            return (
-                <div class="row">
-                    {cartList.map((cart) =>
-                    <div class="col-sm-3 p-3">
-                        <Card  style={{ width: '20rem' }}>
-                        <Card.Img variant="top" src={cart.imageUrl} height="200vw"/>
-                        <Card.Body>
-                            <Card.Title>{cart.name}</Card.Title>
-                            <Card.Text>
-                            <p>Price: {cart.cost}</p>
-                            <p>Calories: {cart.calories}</p>
-                            </Card.Text>
-                        </Card.Body>
-                        </Card>
-                        </div>
-                    )}
-
-                </div>
+        const items = []
+        var i
+        for(i = 0; i < CartArray.length; i++){
+            items.push(
+            <div class ='col-sm-3 p-3'>
+                <Card  style={{ width: '20rem' }}>
+                <Card.Img variant="top" src={CartArray[i].imageUrl} height="200vw"/>
+                <Card.Body >
+                    <Card.Title>{CartArray[i].name}</Card.Title>
+                    <Card.Text>
+                    <p class='price'>Price: {CartArray[i].cost}</p>
+                    <p class='calories'>Calories: {CartArray[i].calories}</p>
+                    </Card.Text>
+                </Card.Body>
+                </Card>
+            </div>
 
             )
         }
-        return (
-            <FoodListCards cartList={this.props.cart} />
-        )
+        return(<div>{items}</div>)
     }
 }
+
+const mapStateToProps = state => {
+    return {
+    }
+}
+
+export default connect(mapStateToProps)(Cart)
