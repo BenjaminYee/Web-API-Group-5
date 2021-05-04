335099024ca9e899f2c257d5feef980d196ba107
An4123 <anvo2016026@gmail.com>
2021-05-03 22:10:01 -0600

    add calorie count and checkout page formatting

--- src/components/cart.js
+++ src/components/cart.js
@@ -8,7 +8,7 @@ import {ceil} from "mathjs";
 import {cartTotal} from '../actions/foodActions'
 import {Nav} from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
-
+import {round} from 'mathjs'
 class Cart extends Component{
 
     componentDidMount(){
@@ -16,7 +16,7 @@ class Cart extends Component{
         dispatch(fetchCharities())
     }
 
-    roundTotal(total) {
+    roundTotal(total, totalC) {
         console.log(total);
         // Get the checkbox
         var checkBox = document.getElementsByName("boxes");
@@ -30,7 +30,7 @@ class Cart extends Component{
             if (checkBox[i].checked == true) {
                 total = ceil(total);
                 text.style.display = "block";
-                valuetotal.innerHTML = "Total price: " + total;
+                valuetotal.innerHTML = "Total price: $" + total + "<br>Total Calories: " + totalC          
                 cartTotal[0] = total
                 console.log(total);
             } else {
@@ -43,11 +43,11 @@ class Cart extends Component{
     render(){
         const items = []
         const charities = []
-
+        var totalCals = 0
         var total = 0
         for(var i = 0; i < CartArray.length; i++){
             items.push(
-            <div class ='col-sm-3 p-3'>
+            <div class ='col-sm-1 p-1'>
                 <Card  style={{ width: '20rem' }}>
                 <Card.Img variant="top" src={CartArray[i].imageUrl} height="200vw"/>
                 <Card.Body >
@@ -61,9 +61,11 @@ class Cart extends Component{
             </div>
 
             )
+            totalCals = totalCals + CartArray[i].calories
             total = total + CartArray[i].cost
+
         }
-        cartTotal[0] = total
+        cartTotal[0] = round(total,  2)
         for (var i = 0; i<this.props.charities.length; i++){
             charities.push(
                 <div className='col-sm-3 p-3'>
@@ -75,17 +77,29 @@ class Cart extends Component{
                                 <p className='description'>{this.props.charities[i].description}</p>
                                 <p id="text" style={{display:"none"}}>charity added!</p>
                             </Card.Text>
-                            <input name="boxes" type="checkbox" id="myCheck" onClick={()=>this.roundTotal(total)}/>
+                            <input name="boxes" type="checkbox" id="myCheck" onClick={()=>this.roundTotal(cartTotal[0], totalCals)}/>
                         </Card.Body>
                     </Card>
                 </div>
             )
         }
 
-        return(<div>
-            <section>{items}</section>
-            <section>{charities}</section>
-            <div><h2 id="valuetotal" ><p style={{display:"block"}}>Total price: {total}</p></h2></div>
+        return(
+        
+        <div class='row'>
+            <section>
+                <p>Cart Items</p>
+                {items}
+            </section>
+            <section>                
+                <p>Pick A Chairty to Donate to</p>
+                {charities}
+            </section>
+            <div><h2 id="valuetotal" >
+                <p style={{display:"block"}}>Total Price: ${cartTotal[0]}</p>
+                <p style={{display:"block"}}>Total Calories: {totalCals}</p>
+                </h2>
+            </div>
             <LinkContainer to={'/checkOut'}>
                 <Nav.Link><button>Check Out</button></Nav.Link>
             </LinkContainer>
